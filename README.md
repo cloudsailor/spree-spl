933e60cd3d213b05f3ddc191887178182165a4a6
nika-piotrowska <w.piotrowska97@gmail.com>
2025-01-17 22:56:48 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Spree SpartaLoyalty
+====================
+
+=======
+Add to your Gemfile:
+
+    gem 'spree-spl'
+
+and run
+
+    bundle install
+========
---
+++ lib/spree/spl.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "spl/version"
+
+module Spree
+  module Spl
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
