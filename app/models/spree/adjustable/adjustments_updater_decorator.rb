e7288b04ee3838911000bbe527cb45f36b86ae55
nika-piotrowska <w.piotrowska97@gmail.com>
2025-03-20 19:30:16 +0100

    Improve adjustments_updater_decorator.rb

--- app/models/spree/adjustable/adjustments_updater_decorator.rb
+++ app/models/spree/adjustable/adjustments_updater_decorator.rb
@@ -6,23 +6,33 @@ module Spree
       def persist_totals(totals)
         attributes = totals
 
-        set_adjustments if @adjustable.is_a?(::Spree::Order) && @adjustable.public_metadata.key?(:spl_card_active)
-
-        if @adjustable.is_a?(::Spree::LineItem) && @adjustable.adjustments.any? { |adj| adj.source_type == "SPL" }
-          recalculate_spl_adjustments(attributes, totals)
-        end
+        set_spree_adjustments if shipment_with_adjustments? || order_with_adjustments?
+        recalculate_spl_adjustments(attributes, totals) if line_item_with_spl_adjustments?
       end
 
       private
 
-      def set_adjustments
-        if @adjustable.public_metadata[:spl_card_active] == true
+      def set_spree_adjustments
+        adjustable = @adjustable.is_a?(::Spree::Order) ? @adjustable : @adjustable.order
+        if adjustable.public_metadata[:spl_card_active] == true
           @adjustable.adjustments.update_all(eligible: false)
         else
           @adjustable.adjustments.update_all(eligible: true)
 
           super
         end
+      end
+
+      def shipment_with_adjustments?
+        @adjustable.is_a?(::Spree::Shipment) && @adjustable.order.public_metadata.key?(:spl_card_active)
+      end
+
+      def order_with_adjustments?
+        @adjustable.is_a?(::Spree::Order) && @adjustable.public_metadata.key?(:spl_card_active)
+      end
+
+      def line_item_with_spl_adjustments?
+        @adjustable.is_a?(::Spree::LineItem) && @adjustable.adjustments.any? { |adj| adj.source_type == "SPL" }
       end
 
       def recalculate_spl_adjustments(attributes, totals)
