6f9588f087fa1de4fed85f8796f7d83a1c57a3f2
nika-piotrowska <w.piotrowska97@gmail.com>
2025-04-15 19:28:47 +0200

    Add promotion check to checkout controller decorator

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -3,6 +3,10 @@
 module CheckoutControllerDecorator
   private
 
+  def promotions_and_spl_adjustment_present?(order)
+    order.promotions.any? && order.line_items.map { |li| li.adjustments.where(source_type: 'SPL') }.present?
+  end
+
   def promotion_switcher(order, check_only)
     PromotionSwitcherService.new(order, check_only).call
   end
