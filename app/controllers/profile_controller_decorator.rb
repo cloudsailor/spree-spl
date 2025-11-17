77e057c5526775b7363e211a612b8e0447ae4235
vi4me <vitalii.cherednichenko@yes.pl>
2025-11-17 09:58:52 +0100

    EK-681 SPL Przeliczanie znizek

---
+++ app/controllers/profile_controller_decorator.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module ProfileControllerDecorator
+  def self.prepended(base)
+    base.before_action :validate_spl_no_card, only: :update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :email,
+                                 public_metadata: [:spl_card_active, :spl_no_card])
+  end
+
+  def validate_spl_no_card # rubocop:disable Metrics/AbcSize
+    return unless user_params[:public_metadata].present? || user_params[:public_metadata][:spl_no_card].present?
+    return if disactivated_card
+
+    ::Spl::ValidateCardService.new(user_params[:public_metadata][:spl_no_card], spree_current_user, current_store).call
+  rescue ::Spl::ValidateCardService::SplCardValidationError => e
+    update_order
+    render json: { error: e.message }, status: :bad_request
+  end
+
+  def update_order(spl_card: nil, active: false)
+    current_order = spree_current_user.orders.last
+    return unless %w[cart address delivery payment].include?(current_order.state)
+
+    current_order.update(
+      public_metadata: current_order.public_metadata.merge(
+        {
+          'spl_no_card' => spl_card,
+          'spl_card_active' => active
+        }
+      )
+    )
+  end
+
+  def disactivated_card
+    user_params[:public_metadata][:spl_card_active].present? && !user_params[:public_metadata][:spl_card_active]
+  end
+end
