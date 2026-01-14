bae075fb6e5659b54fd15d2abef31894bec432a9
vi4me <vitalii.cherednichenko@yes.pl>
2026-01-14 15:03:05 +0100

    EK-681 Restore handle_validation_error method without render

--- app/controllers/spl/spree/storefront/profile_controller_decorator.rb
+++ app/controllers/spl/spree/storefront/profile_controller_decorator.rb
@@ -24,13 +24,17 @@ module Spl
           update_order(metadata[:spl_no_card], metadata[:spl_card_active])
           validate_card(metadata)
         rescue ::Spl::ValidateCardService::SplCardValidationError => e
-          flash[:error] = e.message
+          handle_validation_error(e)
         end
 
         def validate_card(metadata)
           return unless disactivated_card?
 
           ::Spl::ValidateCardService.new(metadata[:spl_no_card], spree_current_user, current_store).call
+        end
+
+        def handle_validation_error(error)
+          flash[:error] = error.message
         end
 
         def update_order(spl_card, active)
