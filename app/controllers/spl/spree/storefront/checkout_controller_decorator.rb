15f9b00477d2d05ea5c7b01d0943468b08b66734
vi4me <vitalii.cherednichenko@yes.pl>
2026-01-16 20:38:16 +0100

    EK-681 Restore changes, change promotion_switcher to after_action

--- app/controllers/spl/spree/storefront/checkout_controller_decorator.rb
+++ app/controllers/spl/spree/storefront/checkout_controller_decorator.rb
@@ -7,26 +7,13 @@ module Spl
         include BooleanHelper
 
         def self.prepended(base)
-          base.before_action :promotion_switcher
+          base.after_action :promotion_switcher
         end
 
         private
 
         def promotion_switcher
           PromotionSwitcherService.new(@order, checkout_state_allowed?).call
-          add_spl_discount_params_to_order(spree_current_user) if checkout_state_allowed?
-        end
-
-        def add_spl_discount_params_to_order(user)
-          return if user.blank?
-          return unless user.public_metadata.key?('spl_no_card') && user.public_metadata.key?('spl_card_active')
-
-          current_order.update(public_metadata: current_order.public_metadata.merge(
-            {
-              'spl_no_card' => user.public_metadata['spl_no_card'],
-              'spl_card_active' => cast_boolean(user.public_metadata['spl_card_active'])
-            }
-          ))
         end
 
         def checkout_state_allowed?
