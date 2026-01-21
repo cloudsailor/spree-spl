ff73c7e30382fad8e562da0f491afd6f9b2f04da
nika-piotrowska <w.piotrowska97@gmail.com>
2026-01-21 15:53:41 +0100

    Uncomment necessery line

--- app/services/spl/sparta_loyalty_service.rb
+++ app/services/spl/sparta_loyalty_service.rb
@@ -43,7 +43,7 @@ module Spl
       {
         ver: 4,
         apiUser: @store.private_metadata['spl_api_user'],
-        # apiToken: @store.private_metadata['spl_api_token'],
+        apiToken: @store.private_metadata['spl_api_token'],
         partnerCode: @store.private_metadata['spl_partner_code'],
         placeCode: @store.private_metadata['spl_place_code'],
         mode: @store.private_metadata['spl_mode'],
